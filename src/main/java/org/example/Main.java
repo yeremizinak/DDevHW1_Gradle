12bc6f92b69ea158a10516154761a71c809bbfcb
yeremizinak <eremizinaekaterina@gmail.com>
2024-01-23 22:19:05 +0200

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) throws JsonProcessingException {
+        User user = new User("Kate", "Yeremizina");
+        ObjectMapper om = new ObjectMapper();
+        System.out.println(om.writeValueAsString(user));
+    }
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,16 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package org.example;
+
+public class User {
+    public String name;
+    public String lastName;
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
